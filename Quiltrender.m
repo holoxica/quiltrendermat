9901158916e86141b60e17c34ec45af9fe67831c
holoxica-jk <jk@holoxica.com>
2022-10-12 23:29:48 +0100

    Cleaned up. Mainloop with key actions for animation, next surface, diagnostics.

--- Quiltrender.m
+++ Quiltrender.m
@@ -1,31 +1,40 @@
 % Show 3D image plots on the Looking Glass 3D Light Field displays, where a
-% quilt corresponding to renders from multiple viewpoints is rendered.
+% quilt corresponding to renders from multiple viewpoints is generated.
 % The quilt is send to a python utility to display quilt using the 
 % HoloPlay driver. 
 %
-% (c) Holoxica Limited, 2022. All rights reserved. 
+% (c) Holoxica Limited, 2022. All rights reserved. www.holoxica.com
 
 clear; 
 close all; 
 
 %% initialise display, driver and python utility
-global Quilt;
+defaultDisplay = "portrait"; 
+
+% Check the python interpreter and utility are present and working. 
+% Note that the utility will require external libraries to be installed
+% from Matlab via:
+% >> system("pip3 install websocket-client cbor2 opencv-python")
+utilityPresent = isfile("holoserverpy.py");
 pe = pyenv;
-if pe.Status == 'NotLoaded'
+if not(utilityPresent)
+    warning("Python holoserver utility not found, please contact Holoxica for this.")
+elseif pe.Status == 'NotLoaded'
     pyenv("ExecutionMode","OutOfProcess","Version","3.9");    
     py.list; % Call a Python function to load interpreter
     py.holoserverpy = py.importlib.reload(py.importlib.import_module('holoserverpy'));
+    utilityPresent = true;
+else
+    utilityPresent = true;
 end
 
-initialise3Ddisplay(); 
-
+global Quilt;
+Quilt = initialise3Ddisplay(utilityPresent,defaultDisplay); 
 Quilt.image = zeros(Quilt.sizepx,Quilt.sizepx,3,"uint8"); 
-Quilt.size = Quilt.rows*Quilt.cols;
-Quilt.imresX = floor(Quilt.sizepx / Quilt.cols); 
-Quilt.imresY = floor(Quilt.sizepx / Quilt.rows); 
 
-% File name format and python commands
-fname = "Matlablogo";
+[f, ax, surfnames, fname] = surfshow(); % generate a 3D surface
+
+% File name format and python command line
 ext = "png";
 quiltstr = strcat('_qs',num2str(Quilt.cols),'x',num2str(Quilt.rows), ...
                   "a",num2str(Quilt.aspect,'%1.2f'));
@@ -33,16 +42,15 @@ fn = strcat(fname, quiltstr, ".", ext); % image file name
 pyscript = "holoserverpy.py";
 cmd = strcat("python3 ", pyscript, " ", fn);
 
-[f, ax] = surfshow(fname);
-
 ax.Interactions = zoomInteraction;
 tb = axtoolbar(ax,"default");
-tb.SelectionChangedFcn = @(src,evt)toolbarSelection(src,evt);
+tb.SelectionChangedFcn = @(src,evt)testCallback(src,evt);
 %f.MenuBar = "none";
 %f.ToolBar = "auto";
 
 %f.WindowButtonUpFcn = "disp('figure callback')";
 f.WindowScrollWheelFcn = "disp('Scroll callback')";
+f.WindowKeyPressFcn = @(src,evt)testCallback(src,evt); %"disp('Key realease callback')";
 f.Position(3:4) = [Quilt.imresX Quilt.imresY]*0.71; % set resolution of viewport
 
 h = rotate3d;
@@ -66,7 +74,9 @@ shared.fn = fn;
 shared.ext = ext; 
 shared.cmd = cmd;
 shared.diagnostic = false;
+shared.keyPressed = ""; 
 
+% A second figure is used for the actual rendering. It is normally invisible
 fig2 = figure;
 fig2.MenuBar = "none";
 %fig2.ToolBar = "none";
@@ -74,20 +84,50 @@ fig2.Color = f.Color;
 fig2.Colormap = f.Colormap;
 fig2.Position(1:2) = f.Position(1:2) + [0 -Quilt.imresY];    
 fig2.Position(3:4) = [Quilt.imresX/2 Quilt.imresY/2]; % set resolution of renderer
-%rotate3d;
-%axis off;
 axis manual;
 hold off;
 shared.fig2 = fig2;
 
+%% Main game loop
 figure(f);
-
 h.Enable = 'on';
+animation = false;
 renderViews("",""); % do a first render
 done = false;
 while not(done)
-    done = not(isvalid(f)); 
-    pause(0.02);
+    done = not(isvalid(h)); 
+    if isvalid(h) h.Enable = 'off'; end
+    pause(0.1);
+    if shared.keyPressed ~= ""
+        
+        switch shared.keyPressed
+            case 'a'    % animation
+                animation = not(animation);
+
+            case 'd'
+                clf(f); 
+                shared.diagnostic = not(shared.diagnostic);
+
+            case 'rightarrow'   % move to next surface !! need to update fname
+                animation = false;
+                clf(f);
+                idx = find(contains(surfnames,fname));  % current index
+                idx = mod(idx,length(surfnames))+1;     % next image
+                fname = surfnames(idx);
+                [f, ax] = surfshow(fname);
+                fig2.Color = f.Color;
+                fig2.Colormap = f.Colormap;
+        end
+        fprintf("Key: %s \n",shared.keyPressed);
+        shared.keyPressed = ""; 
+        renderViews("",""); % force a render
+    end
+    if isvalid(h) h.enable = 'on'; end
+    pause(0.2)
+    if animation
+        figure(f);
+        ax.View = ax.View + [2 0]; 
+    end
 end
 imwrite(Quilt.image,shared.fn,shared.ext); % write out the quilt for testing
 fprintf('Quilt written to: %s \n',fn);
@@ -101,7 +141,6 @@ function renderViews(src,evt)
     f = gcf;
     ax = gca(f);
     h = rotate3d(f);
-    %campos = ax.CameraPosition; 
     setAllowAxesRotate(h,ax,false); % disable rotation during renders
     clf(shared.fig2); 
     copyobj(ax,shared.fig2);
@@ -131,43 +170,39 @@ function renderViews(src,evt)
         np_quilt = py.numpy.array(Quilt.image); 
         py.holoserverpy.mat_quilt(np_quilt,Quilt.cols,Quilt.rows,Quilt.aspect); 
     end
+    %h.Enable = 'on';
     setAllowAxesRotate(h,ax,true); % enable rotating
     % imwrite(Quilt.image,shared.fn,shared.ext);     % write to disk
     % status = system(shared.cmd)                   % call python via cmd line
 end
 
 
-function toolbarSelection(src,evt,~)
-    disp(src);
-    disp(evt);
-
+function testCallback(src,evt,~)
+    %disp(src);
+    %disp(evt);
+    global shared;    
+    shared.keyPressed = evt.Key; % register this for the mainloop to pick up
 end
 
 
-%% Initialise the 3D display using the python utility
-function initialise3Ddisplay()
-    clear py
-    global Quilt;
+%% Initialise the Looking Glass 3D display using the python utility
+function Quilt = initialise3Ddisplay(utilityPresent,defaultDisplay)
+% Initialise the 3D display via the python utility that returns info
+% from the HoloPlay driver. The info contains all of the parameters related
+% to the quilt and the display itself. If this display is not connected
+% then a default set of parameters is used.
+
+    % global Quilt;
     Quilt.displayPresent = false;
     status = {false}; 
-    LKG_display = "portrait"; % can be '15.6' or '32', used for default if no display connected, otherwise get info from driver
+    LKG_display = defaultDisplay; % can be '15.6' or '32', used for default if no display connected, otherwise get info from driver
 
-    %    pe = pyenv;
-%     % Check the python utility is present and working
-%     if not(isfile("holoserverpy.py"))
-%         warning("Python holoserver utility not found, please contact Holoxica for this.")
-%     elseif pe.Status == 'NotLoaded'
-%         pyenv("ExecutionMode","OutOfProcess","Version","3.9");       
-%         py.list; % Call a Python function to load interpreter
-%     else
-%         py.list
-%     end
-%    py.holoserverpy = py.importlib.reload(py.importlib.import_module('holoserverpy'));
-
-    try
-        status = py.holoserverpy.ws_init(); % initialise contact with utility
-    catch
-        warning("Holoplay driver is not installed or running, please fix")
+    if utilityPresent
+        try
+            status = py.holoserverpy.ws_init(); % initialise contact with utility
+        catch
+            warning("Holoplay driver is not installed or running, please fix")
+        end
     end
 
     if status{1}==false
@@ -188,7 +223,7 @@ function initialise3Ddisplay()
             calibration = struct(params.calibration);
             viewCone = struct(calibration.viewCone);
             Quilt.viewCone = double(viewCone.value);
-            LKG_display = ""; 
+            LKG_display = string(params.hardwareVersion); 
             Quilt.displayPresent = true;
         end 
     end
@@ -211,16 +246,36 @@ function initialise3Ddisplay()
             Quilt.viewCone = 53;        
     end
     if LKG_display == "8k" Quilt.sizepx = 8192; end
+    Quilt.LKG_display = LKG_display;
 
+    % derived parameters, useful later on
+    Quilt.size = Quilt.rows*Quilt.cols;
+    Quilt.imresX = floor(Quilt.sizepx / Quilt.cols); 
+    Quilt.imresY = floor(Quilt.sizepx / Quilt.rows); 
 
 end
 
 
 %% Define different kinds of images to show
 
-function [f, ax] = surfshow(fname)
+function [f, ax, surfnames, name] = surfshow(varargin)
+    
+    surfnames = ["Matlablogo", "Peaks", "Worldmap"];
+    if nargin == 0
+        name = surfnames(1);
+    elseif nargin == 1 
+        if sum(contains(surfnames,varargin{1})) == 0
+            name = surfnames(1);
+        else
+            name = varargin{1};
+        end
+    else 
+        name = surfnames(1);
+    end
 
-    switch fname
+    f = gcf;
+
+    switch name
 
         case "Matlablogo"
             L = 1000*membrane(1,100);
@@ -229,6 +284,7 @@ function [f, ax] = surfshow(fname)
             view(3);
             % Colours & map
             s.FaceColor = [0.9 0.2 0.2];
+            colormap("default");
             
             % Specular reflections
             s.FaceLighting = 'gouraud';
@@ -253,14 +309,14 @@ function [f, ax] = surfshow(fname)
             l3.Position = [60 -300 80];
             l3.Style = 'local';
             l3.Color = [0 0.8 0.8];
-
+            
             axis off;
-            f = gcf;
             f.Color = "black";
 
         case "Peaks"
             s = surf(peaks);
             colormap("hsv");
+            f.Color = "white";
 
         case "Worldmap"   
             [x,y,z] = sphere(50);          % create a sphere 
@@ -274,6 +330,8 @@ function [f, ax] = surfshow(fname)
             
             light('Position',[-1 0 1])     % add a light
             axis square off                % set axis to square and remove axis
+            f.Color = "white"
+            colormap('default'); 
 
     end
 
@@ -287,7 +345,5 @@ function [f, ax] = surfshow(fname)
     ax.CameraPositionMode = "manual";
     ax.CameraTargetMode = "manual";
     ax.CameraViewAngleMode = "manual";
-
-    
 
 end
